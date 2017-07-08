d21efc7e70317a018c20740ee57d078c4c7a6a08
MAOKG <maosen1993@gmail.com>
2017-07-08 20:41:12 +0000

    Fix req os

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express')
     app = express()
     bodyParser = require('body-parser')
+    requestIp = require('request-ip')
+    useragent = require('express-useragent')
 
-
+app.use(useragent.express())
+app.use(requestIp.mw())
 app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', function(req, res) {
 
 
-  res.send({'ipaddress': req.ip,
-            'software': req.get('user-agent'),
+  res.send({'ipaddress': req.clientIp,
+            'operating system': req.useragent.os,
             'language': req.acceptsLanguages()[0]
            })
 })
