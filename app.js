b29e4a7449eafac88b950fa2cad29a507e876d4a
MAOKG <maosen1993@gmail.com>
2017-07-08 20:07:06 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express')
+    app = express()
+    bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.get('/', function(req, res) {
+
+
+  res.send({'ipaddress': req.ip,
+            'software': req.get('user-agent'),
+            'language': req.acceptsLanguages()[0]
+           })
+})
+
+
+app.listen(3000, function() {
+  console.log('The server has started!')
+})
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//    console.log('The Server Has Started!');
+// });
